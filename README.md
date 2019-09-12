011455693ff8b01a0b7c8ebc0b8197cb33e5591f
David Dale <ddale@yandex-team.ru>
2019-09-12 23:18:44 +0300

    translate the skill into Russian; write a detailed readme

--- README.md
+++ README.md
@@ -1,13 +1,49 @@
 
+# tgalice + YC Serverless = <3
 
+Этот репозиторий показывает, как можно разработать простенький stateful навык для Алисы 
+на основе [serverless functions](https://cloud.yandex.ru/docs/serverless-functions/) (далее SF) 
+из Яндекс.Облака и питонячьей библиотеки [tgalice](https://github.com/avidale/tgalice). 
+В качестве "начинки" навыка у нас будет шуточный гороскоп - заодно покажем, как на `tgalice` можно делать
+простенький form-filling.
+
+Почему всё устроено так, как устроено:
+* Для формирования среды выполнения функции в SF сейчас вручную нужно собирать все зависимости - 
+это делает `Makefile` (но запускать его надо под Ubuntu)
+* CLI интерфейс SF не позволяет загружать сборки более четырёх мегабайт - поэтому сборку предварительно
+нужно залить в [Object Storage](https://cloud.yandex.ru/docs/storage/), и добавлять её в функции уже оттуда. 
+* Object Storage нам понадобиться в любом случае - там будет храниться состояние диалога.
+
+Что нужно сделать предварительно:
+* Зарегистрироваться в Я.Облаке: https://cloud.yandex.ru
+* Зарегистрироваться в [превью SF](https://cloud.yandex.ru/services/serverless-functions)
+* Создать себе два бакета в [Object Storage](https://cloud.yandex.ru/docs/storage/), назвать их `{BUCKET NAME}` 
+и `tgalice-test-state-storage` (вот это второе имя сейчас захардкожено в `main.py`)
+* Создать [сервисный аккаунт](https://cloud.yandex.ru/docs/iam/concepts/users/service-accounts),
+дать ему роль `editor`, и получить к нему статические креденшалы `{KEY ID}` и `{KEY VALUE}` 
+- их будем использовать для записи состояния диалога. 
+* Установить интерфейс командной строки `yc`
+
+Фух, теперь можно, собственно, деплоить функцию:
+
+```
 make all
-# upload dist.zip
-yc serverless function version create
-    --function-name=horoscope
-    --environment=k1=v1,AWS_ACCESS_KEY_ID={KEY ID},AWS_SECRET_ACCESS_KEY={KEY VALUE}
-    --runtime=python37
-    --package-bucket-name=firstbucket
-    --package-object-name=dist.zip
-    --entrypoint=main.alice_handler
-    --memory=128M
+# теперь надо ручками загрузить dist.zip в бакет OBJECT STORAGE, из которого будем деплоиться 
+yc serverless function version create\
+    --function-name=horoscope\
+    --environment=AWS_ACCESS_KEY_ID={KEY ID},AWS_SECRET_ACCESS_KEY={KEY VALUE}\
+    --runtime=python37\
+    --package-bucket-name={BUCKET NAME}\
+    --package-object-name=dist.zip\
+    --entrypoint=main.alice_handler\
+    --memory=128M\
     --execution-timeout=3s
+```
+После того, как функция запущена, протестировать работу навыка можно подобно тому, как описано в 
+[официальном примере](https://cloud.yandex.ru/docs/serverless-functions/solutions/alice-skill):
+дописать к урлу функции `?integration=raw` и использовать это в качестве вебхука.
+
+Этот пример написан по мотивам [примера от Глеба](https://github.com/monsterzz/yc-serverless-python-deps) 
+(Глеб, спасибо тебе за работу в ночи!). Любые комментарии и правки принимаются.
+
+(В частности, интересно будет попробовать разогнать Object Storage - пока что он мальца подтормаживает)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import random
 import tgalice
 
 import boto3
@@ -9,18 +10,32 @@ from tgalice.storage.session_storage import BaseStorage
 from botocore.exceptions import ClientError
 
 
-DEFAULT_MESSAGE = 'Hi! Please say "start" and I will tell you your future. k1={}'.format(os.environ['k1'])
+DEFAULT_MESSAGE = 'Привет! Вы в навыке "Дурацкий гороскоп". ' \
+                  'Скажите "Старт", чтобы узнать, что сулят вам звёзды. ' \
+                  'Скажите "Выход", чтобы покинуть навык.'
+
+FORECASTS = [
+    'не выкатывате релизы в пятницу, и вас жду отличные выходные!',
+    'возможно, пришло время избавиться от сотен непрочитанных сообщений. Чистый инбокс - к чистоте в карме!',
+    'на этой неделе вам надо обязательно побывать в Парке Горького. '
+    'Вероятно, вы встретите там человека, который изменит вашу жизнь.',
+    'кажется, в вашей жизни близится время перемен. Может быть, это будет переход на Яндекс.Облако?',
+]
 
 
 class S3BasedStorage(BaseStorage):
-    def __init__(self, s3_client, bucket_name):
+    def __init__(self, s3_client, bucket_name, prefix=''):
         super(BaseStorage, self).__init__()
         self.s3_client = s3_client
         self.bucket_name = bucket_name
+        self.prefix = prefix
+
+    def modify_key(self, key):
+        return self.prefix + key
 
     def get(self, key):
         try:
-            result = self.s3_client.get_object(Bucket=self.bucket_name, Key=key)
+            result = self.s3_client.get_object(Bucket=self.bucket_name, Key=self.modify_key(key))
             body = result['Body']
             reader = codecs.getreader("utf-8")
             return json.load(reader(body))
@@ -31,58 +46,63 @@ class S3BasedStorage(BaseStorage):
                 raise e
 
     def set(self, key, value):
-        self.s3_client.put_object(Bucket=self.bucket_name, Key=key, Body=json.dumps(value))
+        self.s3_client.put_object(Bucket=self.bucket_name, Key=self.modify_key(key), Body=json.dumps(value))
 
 
 class CheckableFormFiller(tgalice.dialog_manager.form_filling.FormFillingDialogManager):
     SIGNS = {
-        'jan': 'The Goat',
-        'feb': 'The Water Bearer',
-        'mar': 'The Fishes',
-        'apr': 'The Ram',
-        'may': 'The Bull',
-        'jun': 'The Twins',
-        'jul': 'The Crab',
-        'aug': 'The Lion',
-        'sep': 'The Virgin',
-        'oct': 'The Balance',
-        'nov': 'The Scorpion',
-        'dec': 'The Archer',
+        'январь': 'Козерог',
+        'февраль': 'Водолей',
+        'март': 'Рыбы',
+        'апрель': 'Овен',
+        'май': 'Телец',
+        'июнь': 'Близнецы',
+        'июль': 'Рак',
+        'август': 'Лев',
+        'сентябрь': 'Дева',
+        'октябрь': 'Весы',
+        'ноябрь': 'Скорпион',
+        'декабрь': 'Стрелец',
     }
 
     def handle_completed_form(self, form, user_object, ctx):
         response = tgalice.dialog_manager.base.Response(
-            text='Thank you, {}! Now we know: you are {} years old and you are probably {}. Lucky you!'.format(
+            text='Спасибо, {}! Теперь мы знаем: вам {} лет, и вы {}. \n'
+                 'Вот это вам, конечно, повезло! Звёзды говорят вам: {}'.format(
                 form['fields']['name'],
                 2019 - int(form['fields']['year']),
-                self.SIGNS[form['fields']['month']]
+                self.SIGNS[form['fields']['month']],
+                random.choice(FORECASTS),
             ),
             user_object=user_object,
         )
         return response
 
 
-session = boto3.session.Session()
-s3 = session.client(
-    service_name='s3',
-    endpoint_url='https://storage.yandexcloud.net',
-    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-    region_name='ru-central1',
-)
-
-storage = S3BasedStorage(s3_client=s3, bucket_name='tgalice-test-state-storage')
-
-
-dm = CheckableFormFiller('/function/code/form.yaml', default_message=DEFAULT_MESSAGE)
-connector = tgalice.dialog_connector.DialogConnector(dialog_manager=dm, storage=storage)
-
-
-def alice_handler(alice_request, context):
-    return connector.respond(alice_request, source=tgalice.SOURCES.ALICE)
-
-
 if __name__ == '__main__':
+    # local run or server-ful deploy
     logging.basicConfig(level=logging.INFO)
+
+    dm = CheckableFormFiller('form.yaml', default_message=DEFAULT_MESSAGE)
+    connector = tgalice.dialog_connector.DialogConnector(dialog_manager=dm)  # by default, store state in RAM
     server = tgalice.flask_server.FlaskServer(connector=connector)
     server.parse_args_and_run()
+else:
+    # serverless deploy
+    session = boto3.session.Session()
+    s3 = session.client(
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+        region_name='ru-central1',
+    )
+
+    storage = S3BasedStorage(s3_client=s3, bucket_name='tgalice-test-state-storage')
+
+    dm = CheckableFormFiller('/function/code/form.yaml', default_message=DEFAULT_MESSAGE)
+    connector = tgalice.dialog_connector.DialogConnector(dialog_manager=dm, storage=storage)
+
+    def alice_handler(alice_request, context):
+        return connector.respond(alice_request, source=tgalice.SOURCES.ALICE)
+
